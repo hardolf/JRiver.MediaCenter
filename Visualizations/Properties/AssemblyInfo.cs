8666463d7776271adec913c0d86390d4667f4b29
Bent <50554809+hardolf@users.noreply.github.com>
2019-06-01 16:44:05 +0200

    Moved the expanded ReleasNotes. Renamed the track info visualization to "Cover, Details and Lyrics"

--- Visualizations/Properties/AssemblyInfo.cs
+++ Visualizations/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Visualizations")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Visualizations, track info etc. for JRiver MediaCenter")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Hardolf")]
 [assembly: AssemblyProduct("Visualizations")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
